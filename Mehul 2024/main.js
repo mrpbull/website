75ea15784c102ffbfb4f87553238cdecfd731022
Mehul Panchal <152465060+mrpbull@users.noreply.github.com>
2024-12-23 02:07:54 +0800

    Add files via upload

---
+++ Mehul 2024/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Fullstack Developer", "PHP Developer"],
+  typeSpeed: 70, // Slower typing speed
+  backSpeed: 50, // Slower backspacing speed
+  backDelay: 1500, // Longer delay before backspacing
+  loop: true,
+});
